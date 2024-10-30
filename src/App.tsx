6e4f3405f3e287924bef93a0f06b1d81a940353c
Rodjxc <rodrysxc@gmail.com>
2024-10-30 12:17:35 +0100

    refactor: changed bg and color for site. Add logo and update favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 export const App = () => {
 	return (
-		<main className="flex justify-center items-center bg-gradient-to-br from-purple-500 via-teal-500 to-blue-500 h-[100vh] w-full">
+		<main className="flex justify-center items-center h-[100vh] w-full">
 			Hello world
 		</main>
 	);
