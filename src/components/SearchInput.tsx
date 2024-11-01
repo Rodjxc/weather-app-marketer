aeb46a8feeaac0bcdc09325db52d69e57f96f5a6
Rodjxc <rodrysxc@gmail.com>
2024-11-01 14:03:42 +0100

    refactor: add functionality to dropdown and interaction to be able to choose location with keyboard keys

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import type { optionType } from "../types";
 import { FaMagnifyingGlass } from "react-icons/fa6";
 
@@ -19,13 +19,39 @@ export const SearchInput = ({
   onSubmit,
   locationError,
 }: SearchInputProps): JSX.Element => {
+  const [activeOptionIndex, setActiveOptionIndex] = useState<number>(-1);
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "ArrowDown") {
+      setActiveOptionIndex((prevIndex) =>
+        prevIndex < options.length - 1 ? prevIndex + 1 : 0
+      );
+    } else if (e.key === "ArrowUp") {
+      setActiveOptionIndex((prevIndex) =>
+        prevIndex > 0 ? prevIndex - 1 : options.length - 1
+      );
+    } else if (e.key === "Enter") {
+      // Select the highlighted option or submit
+      if (activeOptionIndex >= 0 && activeOptionIndex < options.length) {
+        onOptionSelect(options[activeOptionIndex]);
+        setActiveOptionIndex(-1); // Reset index after selection
+      } else {
+        onSubmit();
+      }
+    }
+  };
+
   return (
     <div className="relative w-full max-w-[600px]">
       <div className="flex items-center">
         <input
           type="text"
           value={location}
-          onChange={onInputChange}
+          onChange={(e) => {
+            onInputChange(e);
+            setActiveOptionIndex(-1); // Reset index when input changes
+          }}
+          onKeyDown={handleKeyDown}
           placeholder="Search for a location..."
           className="w-full px-4 py-2 rounded-l-md border-2 border-white text-black"
         />
@@ -39,14 +65,18 @@ export const SearchInput = ({
           <FaMagnifyingGlass />
         </button>
       </div>
-      {options.length > 0 && (
+      {options.length > 0 && location && (
         <ul className="absolute top-12 left-0 w-full bg-white rounded-b-md shadow-md text-gray-800 z-10">
           {options.map((option, index) => (
             <li key={index}>
               <button
                 type="button"
                 onClick={() => onOptionSelect(option)}
-                className="block w-full text-left px-4 py-2 hover:bg-blue-500 hover:text-white"
+                className={`block w-full text-left px-4 py-2 ${
+                  index === activeOptionIndex
+                    ? "bg-zinc-700 text-white"
+                    : "hover:bg-zinc-700 hover:text-white"
+                }`}
               >
                 {option.name}, {option.country}
               </button>
