b118af204b2b6d9d779b0fbe6efd64aa32b1c2c8
Rodjxc <rodrysxc@gmail.com>
2024-11-01 14:17:53 +0100

    feat: add a simple footer

---
+++ src/components/Layout/Footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <footer className="w-full h-20 flex items-center justify-center">
+      <p className="text-white text-sm">Developed by RodJDev</p>
+    </footer>
+  );
+};
